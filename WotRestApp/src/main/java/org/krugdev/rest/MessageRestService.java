71674925f1ad3ad3ad20c5c7febe5f4af257029e
Marcin Kruglik <blekione@gmail.com>
2016-05-26 21:45:40 +0100

    Initial commit with initial RESTEasy application
    Creates directory structure and sample echo application to check if REST can be deployed to WildFly server container.

---
+++ WotRestApp/src/main/java/org/krugdev/rest/MessageRestService.java
@@ -0,0 +1,20 @@
+package org.krugdev.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/message")
+public class MessageRestService {
+
+	@GET
+	@Path("/{param}")
+	public Response printMessage(@PathParam("param") String msg) {
+
+		String result = "Restful example : " + msg;
+
+		return Response.status(200).entity(result).build();
+
+	}	
+}
