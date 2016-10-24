131ed828751b7fc06ec0a73347985337f675ecad
Marcin Kruglik <blekione@gmail.com>
2016-10-24 05:56:41 +0100

    Replaces JSONParserUtils class with new JSOnToObjectBuilder
    To achieve better decoupling from GSON and better separation of concerns

--- WotRestApp/src/main/java/org/krugdev/domain/player/PlayerProcessor.java
+++ WotRestApp/src/main/java/org/krugdev/domain/player/PlayerProcessor.java
@@ -1,6 +1,6 @@
 package org.krugdev.domain.player;
 
-import org.krugdev.auxiliary.JSONParserUtils;
+import org.krugdev.auxiliary.JSONToObjectBuilder;
 import org.krugdev.auxiliary.Platform;
 import org.krugdev.auxiliary.RequestingServices;
 import org.krugdev.auxiliary.ResourceNotFoundException;
@@ -8,38 +8,35 @@ import org.krugdev.auxiliary.WotWebsiteRequest;
 import org.krugdev.domain.player.JSONDataBeans.PlayerClanJSONBean;
 import org.krugdev.domain.player.JSONDataBeans.PlayerJSONBean;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
 public class PlayerProcessor {
 	
 	public static Player getFromAPI(Platform platform, int playerId) 
 			throws ResourceNotFoundException {
+		String playerIdStr = Integer.toString(playerId);
 		Player player = new Player(platform, playerId);
-		player.populateWithData(getPlayerDataFromWotApi(platform, playerId));
-		player.setPlayerClan(getPlayerClanDataFromWotAPI(platform, playerId));
+		player.populateWithData(getPlayerDataFromWotApi(platform, playerIdStr));
+		player.setPlayerClan(getPlayerClanDataFromWotAPI(platform, playerIdStr));
 		return player;
 	}
 	
-	private static PlayerJSONBean getPlayerDataFromWotApi(Platform platform, int playerId) throws ResourceNotFoundException {		
-		JsonObject playerJson = 
-				getJsonFromWot(RequestingServices.PLAYER_PROFILE, platform, playerId).getAsJsonObject();
-		PlayerJSONBean playerJsonBean = JSONParserUtils.getElement(playerJson, new PlayerJSONBean());
+	private static PlayerJSONBean getPlayerDataFromWotApi(Platform platform, String playerId) 
+			throws ResourceNotFoundException {		
+		String jsonString = getJsonString(platform, RequestingServices.PLAYER_PROFILE, playerId);
+		JSONToObjectBuilder<PlayerJSONBean> builder = new JSONToObjectBuilder<>(new PlayerJSONBean());
+		PlayerJSONBean playerJsonBean = builder.fromString(jsonString).withId(playerId).build();
 		return playerJsonBean;
 	}
 	
-	private static PlayerClanJSONBean getPlayerClanDataFromWotAPI(Platform platform, int playerId) throws ResourceNotFoundException {
-		JsonObject playerClanJSON = 
-				getJsonFromWot(RequestingServices.PLAYER_CLAN, platform, playerId).getAsJsonObject();
-		PlayerClanJSONBean playerClanJSONBean = 
-				JSONParserUtils.getElement(playerClanJSON, new PlayerClanJSONBean());
+	private static PlayerClanJSONBean getPlayerClanDataFromWotAPI(Platform platform, String playerId) 
+			throws ResourceNotFoundException {
+		String jsonString = getJsonString(platform, RequestingServices.PLAYER_CLAN, playerId);
+		JSONToObjectBuilder<PlayerClanJSONBean> builder = new JSONToObjectBuilder<>(new PlayerClanJSONBean());
+		PlayerClanJSONBean playerClanJSONBean = builder.fromString(jsonString).withId(playerId).build();
 		return playerClanJSONBean;
 	}
-
-	private static JsonElement getJsonFromWot(RequestingServices service, Platform platform, int playerId) 
-			throws ResourceNotFoundException {
+	
+	private static String getJsonString(Platform platform, RequestingServices service, String id) {
 		WotWebsiteRequest request = new WotWebsiteRequest(platform, service);
-		String playerProfileJsonAsString = request.getJsonFromWotAPI(Integer.toString(playerId));
-		return JSONParserUtils.trimJsonFromRedundantData(playerProfileJsonAsString, Integer.toString(playerId));
+		return request.getJsonFromWotAPI(id);
 	}
 }
--- WotRestApp/src/main/java/org/krugdev/domain/playerTanks/TankItemsProcessor.java
+++ WotRestApp/src/main/java/org/krugdev/domain/playerTanks/TankItemsProcessor.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.krugdev.auxiliary.JSONParserUtils;
+import org.krugdev.auxiliary.JSONToObjectBuilder;
 import org.krugdev.auxiliary.Platform;
 import org.krugdev.auxiliary.RequestingServices;
 import org.krugdev.auxiliary.ResourceNotFoundException;
 import org.krugdev.auxiliary.WotWebsiteRequest;
 import org.krugdev.domain.playerTanks.JSONDataBeans.TankJSONBean;
-
-import com.google.gson.JsonArray;
 
 public class TankItemsProcessor{
 
@@ -23,9 +21,9 @@ public class TankItemsProcessor{
 
 	private static List<TankItem> populateWithData(Platform platform, int playerId) 
 			throws ResourceNotFoundException {
-		
-		String jsonTanksString = getJsonWithTanksFromWotAPI(platform, playerId);
-		List<TankJSONBean> wotAPITanks = getTanksFromJSONString(jsonTanksString, playerId);
+		String playerIdStr = Integer.toString(playerId);
+		String jsonTanksString = getJsonWithTanksFromWotAPI(platform, playerIdStr);
+		List<TankJSONBean> wotAPITanks = getTanksFromJSONString(jsonTanksString, playerIdStr);
 		List<TankItem> tankItems = new ArrayList<>();
 		for(TankJSONBean tankJSONBean : wotAPITanks) {
 			TankItem tankItem = new TankItem();
@@ -35,15 +33,14 @@ public class TankItemsProcessor{
 		return tankItems;
 	}
 
-	private static String getJsonWithTanksFromWotAPI(Platform platform, int playerId) {
+	private static String getJsonWithTanksFromWotAPI(Platform platform, String playerId) {
 		WotWebsiteRequest request = new WotWebsiteRequest(platform, RequestingServices.PLAYER_TANKS);
-		return request.getJsonFromWotAPI(Integer.toString(playerId));
+		return request.getJsonFromWotAPI(playerId);
 	}
 	
-	private static List<TankJSONBean> getTanksFromJSONString(String jsonTanksString, int playerId) throws ResourceNotFoundException {
-		JsonArray tanksJsonArray = 
-				JSONParserUtils.trimJsonFromRedundantData(jsonTanksString, Integer.toString(playerId)).getAsJsonArray();
-		TankJSONBean[] tanksWotApi = JSONParserUtils.getElement(tanksJsonArray, new TankJSONBean[1]);
+	private static List<TankJSONBean> getTanksFromJSONString(String jsonTanksString, String playerId) throws ResourceNotFoundException {
+		JSONToObjectBuilder<TankJSONBean[]> builder = new JSONToObjectBuilder<>(new TankJSONBean[1]);
+		TankJSONBean[] tanksWotApi = builder.fromString(jsonTanksString).withId(playerId).build();
 		return Arrays.asList(tanksWotApi);
 	}
 }
