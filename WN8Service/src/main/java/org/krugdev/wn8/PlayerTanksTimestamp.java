0351302c80992425775bbdcda9beb2ade818b4df
Marcin Kruglik <blekione@gmail.com>
2017-02-16 19:16:50 +0000

    Starts implementing persistence for WN8Service

--- WN8Service/src/main/java/org/krugdev/wn8/PlayerTanksTimestamp.java
+++ WN8Service/src/main/java/org/krugdev/wn8/PlayerTanksTimestamp.java
@@ -1,12 +1,14 @@
 package org.krugdev.wn8;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import org.krugdev.wn8.PlayerTanks;
 
@@ -22,12 +24,16 @@ public class PlayerTanksTimestamp implements PlayerTanks {
 	private int playerId;
 	@Column
 	private LocalDateTime timestamp;
+	@Transient
+	private List<TankItem> tankItems;
 	
 	// default constructor required by persistence
 	protected PlayerTanksTimestamp() {}
 
-	public PlayerTanksTimestamp(int playerId) {
+	public PlayerTanksTimestamp(int playerId, List<TankItem> tankItems) {
 		this.playerId = playerId;
+		this.tankItems = tankItems;
 		this.timestamp = LocalDateTime.now();
 	}
+	
 }
--- WN8Service/src/test/java/org/krugdev/reader/DatabaseReaderTest.java
+++ WN8Service/src/test/java/org/krugdev/reader/DatabaseReaderTest.java
@@ -3,6 +3,7 @@ package org.krugdev.reader;
 import static org.junit.Assert.*;
 
 import java.io.Writer;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,13 +12,17 @@ import javax.persistence.Persistence;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.krugdev.io.Reader;
 import org.krugdev.io.WN8DOARepository;
+import org.krugdev.io.WotAPIServiceReader;
 import org.krugdev.wn8.PlayerTanks;
 import org.krugdev.wn8.PlayerTanksTimestamp;
+import org.krugdev.wn8.TankItem;
 
 public class DatabaseReaderTest {
 
 	private final String PERSISTANCE_UNIT = "testwn8";
+	public static final String SERVICE_URI_LOCATION = "http://localhost:8080/WotRestApp/wotAPI/XBOX-plt/players/";
 	private final int PLAYER_ID = 6479371;
 	private EntityManagerFactory emf;
 	private EntityManager em;
@@ -35,9 +40,11 @@ public class DatabaseReaderTest {
 	}
 	
 	@Test
-	public void shouldAddPlayerTanksToDB() {
+	public void shouldAddPlayerTanksTimestampToDB() {
 		WN8DOARepository reader = new WN8DOARepository(em);
-		PlayerTanks player = new PlayerTanksTimestamp(PLAYER_ID);
+		Reader serviceReader = new WotAPIServiceReader(SERVICE_URI_LOCATION);
+		List<TankItem> tankItems = serviceReader.getPlayerTanks(PLAYER_ID);
+		PlayerTanks player = new PlayerTanksTimestamp(PLAYER_ID, tankItems);
 		reader.savePlayerTanks(player);
 	}
 }
