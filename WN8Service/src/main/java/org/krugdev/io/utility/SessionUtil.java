a78321279631804c7eb45d5c050efe5ca1fa5d8e
Marcin Kruglik <blekione@gmail.com>
2017-02-19 20:25:35 +0000

    Add utility class for hibernate persistence session

---
+++ WN8Service/src/main/java/org/krugdev/io/utility/SessionUtil.java
@@ -0,0 +1,39 @@
+package org.krugdev.io.utility;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.Session;
+
+public class SessionUtil {
+
+	private static final EntityManagerFactory DEFAULT_EMF = Persistence.createEntityManagerFactory("testwn8");
+	private final static SessionUtil instance = new SessionUtil();
+	private EntityManagerFactory emf;
+	
+	private SessionUtil() {
+		this.emf = DEFAULT_EMF;
+	}
+	
+	public static SessionUtil getInstance() {
+		return instance;
+	}
+	
+	public static Session getSession() {
+		return getInstance().emf.createEntityManager().unwrap(Session.class);
+	}
+	
+	public static Session getSession(EntityManagerFactory emf) {
+		getInstance().setEmf(emf);
+		return getSession();
+	}
+
+	public static Session getSession(EntityManager em) {
+		return em.unwrap(Session.class);
+	}
+	
+	private void setEmf(EntityManagerFactory emf) {
+		this.emf = emf;
+	}
+}
